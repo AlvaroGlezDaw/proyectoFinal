49c1c80d808e9ca5d33be36170a62a6fdf4056c8
ggi83428@educastur.es <ggi83428@educastur.es>
2022-06-20 23:04:12 +0200

    Finalizado el responsive para web para resoluciones grandes, medias y pequeñas

--- js/login/loginFunctions.js
+++ js/login/loginFunctions.js
@@ -1,7 +1,7 @@
 function loginChangeNav(){
     if(localStorage.getItem('userLogued')!==null){
         document.getElementById("loginRegister").setAttribute("class", "d-none");
-        document.getElementById("loginDates").setAttribute("class", "col-3 d-block");
+        document.getElementById("loginDates").setAttribute("class", "col-5 d-block");
         document.getElementById("userName").textContent="Bienvenido " + JSON.parse(localStorage.getItem('userLogued')).nombre;
         if(JSON.parse(localStorage.getItem('userLogued')).role!=="user"){
             document.getElementById("panelAdmin").setAttribute("class", "d-inline-block fs-4 bg-warning text-dark border rounded-pill border-dark font-monospace p-1");
@@ -14,7 +14,7 @@ function loginChangeNav(){
     else{
         document.getElementById("loginDates").setAttribute("class", "d-none");
         document.getElementById("userName").textContent="";
-        document.getElementById("loginRegister").setAttribute("class", "col-5 d-block");
+        document.getElementById("loginRegister").setAttribute("class", "col-7 col-md-5 d-block");
     }
 }
 
